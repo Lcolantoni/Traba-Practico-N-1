bc1595d62d6e41e57a218bb50d46a4a5516191b0
Lcolantoni <137439108+Lcolantoni@users.noreply.github.com>
2024-03-19 10:59:11 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import RedirectResponse
 import pandas as pd
 from sklearn.metrics.pairwise import cosine_similarity
-from starlette.responses import RedirectResponse
+
 app = FastAPI()
 
-
-async def root():
-    return RedirectResponse(url="/docs")
+@app.middleware("http")
+async def redirect_root_to_docs(request: Request, call_next):
+    if request.url.path == "/":
+        return RedirectResponse(url="/docs", status_code=301)
+    response = await call_next(request)
+    return response
 
 df_PlayTimeGenre = pd.read_csv('PlayTimeGenre.csv')
 df_UsersForGenre = pd.read_csv('UserForGenre.csv')
